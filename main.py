6934642a63813bf0418b78dd0ae80302de4464b2
chenyangyin95 <chenyangyin95@gmail.com>
2026-02-14 16:49:45 +0800

    feat: / 支持POST并根据file字段URL自动下载解析文件

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
-from fastapi import FastAPI, UploadFile, File
+
+
+from fastapi import FastAPI
+from pydantic import BaseModel
 import pandas as pd
 from docx import Document
 import io
+import requests
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"status": "running"}
+class FileInput(BaseModel):
+    file: str
 
-@app.post("/read_file")
-async def read_file(file: UploadFile = File(...)):
-    content = await file.read()
-    filename = file.filename.lower()
+@app.post("/")
+async def read_root(input_data: FileInput):
+    file_url = input_data.file
+    try:
+        r = requests.get(file_url)
+        r.raise_for_status()
+        content = r.content
+        filename = file_url.split("?")[0].split("/")[-1].lower()
+    except Exception as e:
+        return {"error": f"file_url download error: {str(e)}"}
 
     # CSV 文件
     if filename.endswith(".csv"):
